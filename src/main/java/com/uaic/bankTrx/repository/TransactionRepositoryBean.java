84c756deb36ffa04720cb21a5dedfe1c9260ff3a
ionut.vacariu <ionut.vacariu123@gmail.com>
2019-03-17 08:59:56 +0200

    integrated real database  + Transactional

--- src/main/java/com/uaic/bankTrx/repository/TransactionRepositoryBean.java
+++
@@ -1,9 +0,0 @@
-package com.uaic.bankTrx.repository;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-
-@Component
-public abstract  class TransactionRepositoryBean implements TransactionRepository
-{
-}
